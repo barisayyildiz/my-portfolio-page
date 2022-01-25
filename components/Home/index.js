4de1cf60ee60d5b0c17532ef45343d131b399ddf
barisayyildiz <barisayyildiz.1799@gmail.com>
2022-01-26 00:58:40 +0300

    Linkler düzeltildi

--- components/Home/index.js
+++ components/Home/index.js
@@ -7,37 +7,35 @@ import { Linkedin, Github, Medium, CV } from '../SocialMediaIcons';
 
 function Home(){
 	return(
-		<Hero>
+		<Hero id="home">
 			<p>Hello, I'm</p>
 			<p>{user.firstname} {user.lastname}</p>
 			<ul>
 				<li>
 					<Link href="https://www.linkedin.com/in/bar%C4%B1%C5%9F-ayy%C4%B1ld%C4%B1z-627704174/">
-						<a>
+						<a target="_blank">
 							<Linkedin/>
 						</a>
 					</Link>
 				</li>
 				<li>
 					<Link href="https://github.com/barisayyildiz">
-						<a>
+						<a target="_blank">
 							<Github/>
 						</a>
 					</Link>
 				</li>
 				<li>
 					<Link href="https://medium.com/@99ayyldzbaris99">
-						<a>
+						<a target="_blank">
 							<Medium/>
 						</a>
 					</Link>
 				</li>
 				<li>
-					<Link href="">
-						<a>
-							<CV/>
-						</a>
-					</Link>
+					<a href="/mycv.pdf/" download={true} target="_blank">
+						<CV/>
+					</a>
 				</li>
 			</ul>
 		</Hero>
